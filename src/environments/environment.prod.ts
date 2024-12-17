b64ac5cf8b35bcd2aa13cfb3c1fa73aaca94746b
Beto8719 <albertodrueda@gmail.com>
2024-12-16 23:08:29 -0700

    final commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
-    production: true,
-    firebaseConfig: {
-      apiKey: "YOUR_API_KEY",
-      authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-      projectId: "YOUR_PROJECT_ID",
-      storageBucket: "YOUR_PROJECT_ID.appspot.com",
-      messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-      appId: "YOUR_APP_ID",
-    },
-  };
-  
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDCD8azeRBRDc3BwRECafc8rHRjKR7GjSE",
+    authDomain: "pizza-manager-30898.firebaseapp.com",
+    projectId: "pizza-manager-30898",
+    storageBucket: "pizza-manager-30898.appspot.com",
+    messagingSenderId: "835944625517",
+    appId: "1:835944625517:web:8447138640f51277169664",
+  },
+};
