4a0976267d861c20fea02e756ea2b54cb79fd2f5
Beto8719 <albertodrueda@gmail.com>
2024-12-16 23:02:45 -0700

    final commit

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-JCMA3KQL.mjs";import"./chunk-7EFHO4RO.mjs";import"./chunk-AVRSS33M.mjs";import"./chunk-GGVYWPAP.mjs";import"./chunk-KB5IWD3E.mjs";import"./chunk-H6KHSOBK.mjs";export{a as default};
